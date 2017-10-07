6b89885942cf24b72f2a2cb40cc3ca4b9bd53727
Sydney Henry <shenry@handycodejob.com>
2017-10-07 19:34:21 +0000

    update models to allow for AnonymousUsers and wallets for site_keys

--- django_coin_hive/models.py
+++ django_coin_hive/models.py
@@ -10,9 +10,14 @@ from model_utils.fields import AutoCreatedField
 
 
 class CoinHiveSite(TimeStampedModel):
+    """
+    CoinHive gives site keys that are only 32 chars. We have a higer limmit
+    as we also support using your own proxy, which will require a full mxr
+    wallet address
+    """
     site_name = models.CharField(max_length=255)
-    site_key = models.CharField(max_length=32)
-    secret_key = models.CharField(max_length=32)
+    site_key = models.CharField(max_length=255)
+    secret_key = models.CharField(max_length=255)
     default = models.BooleanField(default=False)
 
     def __str__(self):
@@ -24,13 +29,16 @@ class CoinHiveUser(TimeStampedModel):
     While we use ForeignKeys to link users, having multiple CoinHiveUsers for
     each django user is not well suported. When looking for the CoinHiveUser,
     we use `.first()`. While a OneToOne relation might be a better fit, we
-    choose to allow for multiple users be supported in the future.
+    choose to allow for multiple users be supported in the future. We also
+    allow for AnonymusUser to create a CoinHiveUser and set user = None
     """
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        null=True
     )
     name = models.UUIDField(
+        "Username for coin-hive",
         db_index=True,
         default=uuid.uuid4,
         editable=False,
